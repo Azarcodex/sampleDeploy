35d46fba06aafc1c2d0885ed34ae0dea9cd5d2e6
Mohammed Azarin vp <azarcoder@gmail.com>
2026-01-05 12:24:01 +0530

    dummy commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello world" });
+  res.status(200).json({ message: "Hello world how are you" });
 });
 
 app.listen(8000, () => {
